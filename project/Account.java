3fc8665e8015b2fabdac904f45449fdb1047089c
itsreverence <github.shakily561@passmail.net>
2026-02-23 17:45:37 -0600

    change account id type to uuid, add validation on account setters, and add validation to account constructor

--- project/Account.java
+++ project/Account.java
@@ -4,15 +4,33 @@ import java.time.Instant;
 import java.util.UUID;
 
 public class Account {
-    private final String accountId = UUID.randomUUID().toString();
+    private final UUID accountId;
     private String accountHolderName;
     private Instant accountHolderBirthDate;
     private Role role;
 
     public Account(String accountHolderName, Instant accountHolderBirthDate, Role role) {
-        this.accountHolderName = accountHolderName;
-        this.accountHolderBirthDate = accountHolderBirthDate;
-        this.role = role;
+        this.accountId = UUID.randomUUID();
+
+        if (accountHolderName == null) {
+            throw new IllegalArgumentException("Account holder name cannot be null");
+        } else if (accountHolderName.isEmpty()) {
+            throw new IllegalArgumentException("Account holder name cannot be empty");
+        } else {
+            this.accountHolderName = accountHolderName;
+        }
+
+        if (accountHolderBirthDate == null) {
+            throw new IllegalArgumentException("Account holder birth date cannot be null");
+        } else {
+            this.accountHolderBirthDate = accountHolderBirthDate;
+        }
+
+        if (role == null) {
+            throw new IllegalArgumentException("Role cannot be null");
+        } else {
+            this.role = role;
+        }
     }
 
     public String getAccountHolderName() {
@@ -20,7 +38,13 @@ public class Account {
     }
 
     public void setAccountHolderName(String accountHolderName) {
-        this.accountHolderName = accountHolderName;
+        if (accountHolderName == null) {
+            throw new IllegalArgumentException("Account holder name cannot be null");
+        } else if (accountHolderName.isEmpty()) {
+            throw new IllegalArgumentException("Account holder name cannot be empty");
+        } else {
+            this.accountHolderName = accountHolderName;
+        }
     }
 
     public Instant getAccountHolderBirthDate() {
@@ -28,7 +52,11 @@ public class Account {
     }
 
     public void setAccountHolderBirthDate(Instant accountHolderBirthDate) {
-        this.accountHolderBirthDate = accountHolderBirthDate;
+        if (accountHolderBirthDate == null) {
+            throw new IllegalArgumentException("Account holder birth date cannot be null");
+        } else {
+            this.accountHolderBirthDate = accountHolderBirthDate;
+        }
     }
 
     public Role getRole() {
@@ -36,10 +64,14 @@ public class Account {
     }
 
     public void setRole(Role role) {
-        this.role = role;
+        if (role == null) {
+            throw new IllegalArgumentException("Role cannot be null");
+        } else {
+            this.role = role;
+        }
     }
 
-    public String getAccountId() {
+    public UUID getAccountId() {
         return accountId;
     }
 
