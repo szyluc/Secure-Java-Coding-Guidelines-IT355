7354098e66674e3c46be2284cf1f083b566f642f
itsreverence <github.shakily561@passmail.net>
2026-02-27 16:19:59 -0600

    add windows & linux run scripts that handles db lib

---
+++ project/run.ps1
@@ -0,0 +1,2 @@
+javac -cp ".;lib/sqlite-jdbc-3.51.2.0.jar" .\*.java
+java -cp ".;lib/sqlite-jdbc-3.51.2.0.jar" Driver
---
+++ project/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+javac -cp ".:lib/sqlite-jdbc-3.51.2.0.jar" ./*.java
+java -cp ".:lib/sqlite-jdbc-3.51.2.0.jar" Driver
