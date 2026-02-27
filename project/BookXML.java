3cfe74d7ad4aad22dc3efa82f73414e0bc185a55
LymphNoder <lfmolin@ilstu.edu>
2026-02-27 14:03:03 -0600

    Update BookXML.java

--- project/BookXML.java
+++ project/BookXML.java
@@ -14,16 +14,31 @@ class BookXML{
     BookXML(){}
 
     //This is what makes the receipt. This takes a Book object and then takes out the
+    void ReadFile(File file){
+        File xmlFile = newFile(file);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(xmlFile);
+
+        NodeList booksList = document.getElementsByTagName("books");
+        for(int i = 0; i < booksList.getLength(); i++){
+            NodeList bookInfo = document.getElementsByTageName("book");
+            ModifyBooks.addBook(new Book(bookInfo.item(0), bookInfo.item(1), bookInfo.item(2)));
+        }
+    }
+    
     void MakeReceipt(Book book){
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.newDocument();
 
-        Element root = document.createElement("Receipt");
+        Element root = document.createElement("books");
         document.appendChild(root);
 
-        Element name = document.createElement("name");
-        name.appendChild(document.createTextNode(book->name));
+        Element book = document.createElement("book");
+        Element title = document.createElement("title");
+        title.appendChild(document.createTextNode(book->title));
         Element author = document.createElement("author");
         author.appendChild(document.createTextNode(book->author));
         Element category = document.createElement("category");
@@ -32,10 +47,12 @@ class BookXML{
         Element date = document.createElement("date");
         date.appendChild(document.createTextNode(now)); 
         
-        root.appendChild(name);
-        root.appendChild(author);
-        root.appendChild(category);
-        root.appendChild(date);
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(category);
+        book.appendChild(date);
+
+        books.appendChild(book);
         
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
