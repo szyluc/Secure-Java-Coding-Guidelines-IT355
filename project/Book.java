6caea693cebf79c4bd5c4402b039e9bc99af5be2
PMarr62 <117673241+PMarr62@users.noreply.github.com>
2026-02-27 12:26:50 -0600

    Added DatabaseController functionality and Book class

---
+++ project/Book.java
@@ -0,0 +1,28 @@
+import java.util.UUID;
+
+class Book {
+    private final UUID bookID;
+    private String bookName;
+    private String bookAuthor;
+    private String bookCategory;
+
+    public Book(String bookName, String bookAuthor, String bookCategory) {
+        this.bookID = UUID.randomUUID(); // assigns an identifier to the book
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookCategory = bookCategory;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookCategory() {
+        return bookCategory;
+    }
+
+}
