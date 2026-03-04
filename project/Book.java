25d161e79aa72b152a81f62942d5f9f309e3bc36
Brady Binkowski <bradybinkowski@outlook.com>
2026-03-04 13:15:57 -0600

    implemented 4 of my rules

--- project/Book.java
+++ project/Book.java
@@ -1,6 +1,7 @@
 import java.util.UUID;
+import java.nio.CharBuffer;
 
-public class Book {
+public class Book implements Cloneable{
     private final UUID bookID;
     private String bookName;
     private String bookAuthor;
@@ -40,6 +41,31 @@ public class Book {
         this.bookName = bookName;
         this.bookAuthor = bookAuthor;
         this.bookCategory = bookCategory;
+    }
+
+    // FIO05-J: Return a read-only buffers so untrusted code cannot modify
+    // the backing char array through the buffer reference.
+    public CharBuffer getTitleBuffer(){
+        return CharBuffer.wrap(bookName.toCharArray()).asReadOnlyBuffer();
+    }
+
+    public CharBuffer getAuthorBuffer(){
+        return CharBuffer.wrap(bookAuthor.toCharArray()).asReadOnlyBuffer();
+    }
+
+    // MET06-J: clone() only calls the private final copyFields() method.
+    // Create a final method so clone can not be overwritten.
+    @Override
+    public Book clone() throws CloneNotSupportedException{
+        Book cloned = (Book) super.clone();
+        cloned.copyFields(this.bookName, this.bookAuthor, this.bookCategory);
+        return cloned;
+    }
+
+    private final void copyFields(String name, String author, String category){
+        this.bookName = name;
+        this.bookAuthor = author;
+        this.bookCategory = category;
     }
 
 }
