11799d0e6dff58c55c213845480ca0ac3b539e95
Kory <176261423+kjkrahulik@users.noreply.github.com>
2026-03-04 18:53:49 -0600

    Added javadoc for CustomResolver.java

--- project/CustomResolver.java
+++ project/CustomResolver.java
@@ -3,7 +3,27 @@ import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+/**
+ * Custom implementation of the EntityResolver interface used to control how
+ * eternal XML entities are resolved during parsing
+ * 
+ * This resolver restricts entity resolution to a predefined set of trusted xml files (admin.xml, books.xml)
+ * Any unknown or untrusted references are blocked by returning empty
+ * which protects XML External entity attacks and unauthorized access
+ */
 class CustomResolver implements EntityResolver {
+  /**
+   * Resolves external XML entities by allowing only trusted/pre-defined local file paths
+   * Unknown are rejected
+   * 
+   * @param publicId the public identifier of the external entity
+   * @param systemId the system ID fo the external entity
+   * @return an link InputSoruce for trusted entities 
+   *        or an empty link to InputSorce for trusted
+   *        or an empty link if entity is not recognized
+   * @throws SAXEception if parsing related error occurs
+   * @throws IOEception if an input/output error occurs
+   */
   public InputSource resolveEntity(String publicId, String systemId)
       throws SAXException, IOException {
 
